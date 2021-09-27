96fc45a7a54da0b6ac22406ecb8caa668fde69c1
Momin94 <momin.shoaib@cheetay.pk>
2021-09-27 18:27:46 +0500

    UI Constraints changes made

--- MovieApp/Controllers/MovieDetailsViewController.swift
+++ MovieApp/Controllers/MovieDetailsViewController.swift
@@ -29,14 +29,46 @@ class MovieDetailsViewController: UIViewController {
         movieDate?.text = movieDateText
         movieOverview?.text = movieOverviewText
         
-        let imageURL = URL(string: "https://image.tmdb.org/t/p/w500/\(String(describing: movieImageText))")
-        movieImage.sd_setImage(with: imageURL)
-        
-        
+//        let imageURL = URL(string: "https://image.tmdb.org/t/p/w500/\(String(describing: movieImageText))")
+//        movieImage.sd_setImage(with: imageURL)
+//
+       
         
     }
     
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let imageURL = URL(string: movieImageText!)
+        
+        
+        movieImage.sd_setImage(with: imageURL) { (image, error, cache, urls) in
+                    if (error != nil) {
+                        // Failed to load image
+                        self.movieImage.image = UIImage(named: "TestImage.jpg")
+                    } else {
+                        // Successful in loading image
+                        self.movieImage.image = image
+                    }
+                }
+        
+//        movieImage.sd_setImage(
+//             with: imageURL,
+//             placeholderImage: UIImage(named: "App-Default"),
+//             options: SDWebImageOptions(rawValue: 0),
+//             completed: {  image, error, cacheType, imageURL in
+//                          // your rest code
+//                    print(error)
+//
+//                }
+//        )
+
+    //    movieImage.sd_setImage(with: imageURL)
+
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
     
     override func viewWillAppear(_ animated: Bool)
     {
